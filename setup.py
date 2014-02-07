f4698dc4da8c0230bfabb14e85855e4817d8b214
Jesse London <jesselondon@gmail.com>
2014-02-06 20:19:29 -0600

    added django 1.6.1 dispatch package, with reference to django.conf.settings removed, as well as tests and setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from __future__ import with_statement
+import os.path
+import re
+
+from setuptools import setup
+
+
+def get_version():
+    dir_ = os.path.dirname(os.path.abspath(__file__))
+    version_pattern = r'''__version__ *\= *['"]([\d.]+)['"]'''
+    with open(os.path.join(dir_, 'dispatch', '__init__.py')) as init:
+        match = re.search(version_pattern, init.read())
+    return match.group(1)
+
+
+setup(
+    name="Paperboy",
+    description="A simple fork of django.dispatch for use as a standalone "
+                "library.",
+    version=get_version(),
+    packages=['dispatch'],
+    install_requires=['six>=1.4.1'],
+    test_suite='tests',
+    maintainer="Jesse London",
+    maintainer_email="jesse@edgeflip.com",
+    url="http://github.com/edgeflip/dispatch",
+    license='BSD',
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ),
+)
